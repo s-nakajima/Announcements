e28a1bbd803bb1fcae88a010575493ea83dd5c7b
宮川 貴子 <info@nekoget.net>
2014-06-09 11:00:12 +0900

    schema add

---
+++ Config/Schema/schema.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * AnnouncementSchema
+ *
+ * @author   Takako Miyagawa <nekoget@gmail.com>
+ * @link     http://www.netcommons.org NetCommons Project
+ * @license  http://www.netcommons.org/license.txt NetCommons License
+ */
+class AnnouncementSchema extends CakeSchema {
+
+	public function before($event = array()) {
+		return true;
+	}
+
+	public function after($event = array()) {
+	}
+
+	public $announcement_blocks = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'block_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'unique'),
+		'send_mail' => array('type' => 'boolean', 'null' => true, 'default' => '0'),
+		'mail_subject' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'mail_body' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'create_user_id' => array('type' => 'integer', 'null' => false, 'default' => '0'),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified_user_id' => array('type' => 'integer', 'null' => false, 'default' => '0'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'announcement_blocks_block_id' => array('column' => 'block_id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+	public $announcement_blocks_parts = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'announcement_block_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'index'),
+		'part_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'can_edit_content' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
+		'can_publish_content' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
+		'can_send_mail' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'create_user_id' => array('type' => 'integer', 'null' => false, 'default' => '0'),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified_user_id' => array('type' => 'integer', 'null' => false, 'default' => '0'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'announcement_blocks_parts_announcement_block_id' => array('column' => array('announcement_block_id', 'part_id'), 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+	public $announcement_data = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'announcement_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'status_id' => array('type' => 'integer', 'null' => false, 'default' => '1', 'length' => 3),
+		'language_id' => array('type' => 'integer', 'null' => false, 'default' => '1'),
+		'is_original' => array('type' => 'integer', 'null' => false, 'default' => '1', 'length' => 1),
+		'translation_engine' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'content' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'create_user_id' => array('type' => 'integer', 'null' => false, 'default' => '0'),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified_user_id' => array('type' => 'integer', 'null' => false, 'default' => '0'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+	public $announcements = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'block_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'unique'),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'create_user_id' => array('type' => 'integer', 'null' => false, 'default' => '0'),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified_user_id' => array('type' => 'integer', 'null' => false, 'default' => '0'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'announcements_block_id' => array('column' => 'block_id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+}
