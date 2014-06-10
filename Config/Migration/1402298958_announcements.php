076dc82717efe5785f568f3944e4495ffdbcf201
宮川 貴子 <info@nekoget.net>
2014-06-11 07:41:26 +0900

    不要ファイル削除

--- Config/Migration/1402298958_announcements.php
+++ Config/Migration/1402298958_announcements.php
@@ -16,37 +16,6 @@ class Announcements extends CakeMigration {
 	public $migration = array(
 		'up' => array(
 			'create_table' => array(
-				'announcement_blocks' => array(
-					'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
-					'block_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'unique'),
-					'send_mail' => array('type' => 'boolean', 'null' => true, 'default' => '0'),
-					'mail_subject' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'mail_body' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
-					'create_user_id' => array('type' => 'integer', 'null' => false, 'default' => '0'),
-					'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
-					'modified_user_id' => array('type' => 'integer', 'null' => false, 'default' => '0'),
-					'indexes' => array(
-						'PRIMARY' => array('column' => 'id', 'unique' => 1)
-					),
-					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
-				),
-				'announcement_blocks_parts' => array(
-					'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
-					'announcement_block_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'index'),
-					'part_id' => array('type' => 'integer', 'null' => false, 'default' => null),
-					'can_edit_content' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
-					'can_publish_content' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
-					'can_send_mail' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
-					'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
-					'create_user_id' => array('type' => 'integer', 'null' => false, 'default' => '0'),
-					'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
-					'modified_user_id' => array('type' => 'integer', 'null' => false, 'default' => '0'),
-					'indexes' => array(
-						'PRIMARY' => array('column' => 'id', 'unique' => 1)
-					),
-					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
-				),
 				'announcement_data' => array(
 					'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
 					'announcement_id' => array('type' => 'integer', 'null' => false, 'default' => null),
