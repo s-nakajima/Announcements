02fcfff6cd740421b562c878a308beaa86730acd
s-nakajima <nakajimashouhei@gmail.com>
2014-12-19 10:48:53 +0900

    'NetCommons.Trackable'処理の組み込み（created_user、modified_userの更新）

--- Model/Announcement.php
+++ Model/Announcement.php
@@ -22,6 +22,15 @@ App::uses('AnnouncementsAppModel', 'Announcements.Model');
 class Announcement extends AnnouncementsAppModel {
 
 /**
+ * use behaviors
+ *
+ * @var array
+ */
+	public $actsAs = array(
+		'NetCommons.Trackable'
+	);
+
+/**
  * Validation rules
  *
  * @var array
@@ -116,20 +125,6 @@ class Announcement extends AnnouncementsAppModel {
 		);
 
 		return parent::beforeValidate($options);
-	}
-
-/**
- * before save
- *
- * @param array $options Options passed from Model::save().
- * @return bool True if the operation should continue, false if it should abort
- */
-	public function beforeSave($options = array()) {
-		if (! isset($this->data[$this->name]['id'])) {
-			$this->data[$this->name]['created_user'] = CakeSession::read('Auth.User.id');
-		}
-		$this->data[$this->name]['modified_user'] = CakeSession::read('Auth.User.id');
-		return true;
 	}
 
 /**
