34593792452c98bb369f566f0e532fe482957490
topaz2 <topaz2.3333@gmail.com>
2015-02-13 15:36:35 +0900

    Tons of specification changes in progress

--- Test/Case/AllAnnouncementsTest.php
+++ Test/Case/AllAnnouncementsTest.php
@@ -31,9 +31,9 @@ class AllAnnouncementsTest extends CakeTestSuite {
 		$task = 'Announcement';
 		var_dump($task);
 		$suite->addTestFile(CakePlugin::path($plugin) . 'Test' . DS . 'Case' . DS . 'Model' . DS . $task . 'Test.php');
-		/* $task = 'AnnouncementsApp'; */
-		/* var_dump($task); */
-		/* $suite->addTestFile(CakePlugin::path($plugin) . 'Test' . DS . 'Case' . DS . 'Controller' . DS . $task . 'Test.php'); */
+		$task = 'AnnouncementsApp';
+		var_dump($task);
+		$suite->addTestFile(CakePlugin::path($plugin) . 'Test' . DS . 'Case' . DS . 'Controller' . DS . $task . 'Test.php');
 		/* $task = 'AnnouncementsControllerError'; */
 		/* var_dump($task); */
 		/* $suite->addTestFile(CakePlugin::path($plugin) . 'Test' . DS . 'Case' . DS . 'Controller' . DS . $task . 'Test.php'); */
