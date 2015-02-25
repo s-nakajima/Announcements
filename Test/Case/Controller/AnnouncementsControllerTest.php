442d723bb4d44fdbebbd458ebddeb86a06fa17fa
topaz2 <topaz2.3333@gmail.com>
2015-02-25 17:52:51 +0900

    Tons of specification changes in progress

--- Test/Case/Controller/AnnouncementsControllerTest.php
+++ Test/Case/Controller/AnnouncementsControllerTest.php
@@ -20,136 +20,136 @@ App::uses('AnnouncementsAppTest', 'Announcements.Test/Case/Controller');
  */
 class AnnouncementsControllerTest extends AnnouncementsAppTest {
 
-/**
- * Expect visitor can access index action
- *
- * @return void
- */
-	public function testIndex() {
-		$this->testAction(
-			'/announcements/announcements/index/1',
-			array(
-				'method' => 'get',
-				'return' => 'view',
-			)
-		);
-		$this->assertTextEquals('Announcements/view', $this->controller->view);
-	}
+	/* /\** */
+	/*  * Expect visitor can access index action */
+	/*  * */
+	/*  * @return void */
+	/*  *\/ */
+	/* 	public function testIndex() { */
+	/* 		$this->testAction( */
+	/* 			'/announcements/announcements/index/1', */
+	/* 			array( */
+	/* 				'method' => 'get', */
+	/* 				'return' => 'view', */
+	/* 			) */
+	/* 		); */
+	/* 		$this->assertTextEquals('Announcements/view', $this->controller->view); */
+	/* 	} */
 
-/**
- * Expect visitor can access view action
- *
- * @return void
- */
-	public function testView() {
-		$this->testAction(
-			'/announcements/announcements/view/1',
-			array(
-				'method' => 'get',
-				'return' => 'view',
-			)
-		);
-		$this->assertTextEquals('view', $this->controller->view);
-	}
+	/* /\** */
+	/*  * Expect visitor can access view action */
+	/*  * */
+	/*  * @return void */
+	/*  *\/ */
+	/* 	public function testView() { */
+	/* 		$this->testAction( */
+	/* 			'/announcements/announcements/view/1', */
+	/* 			array( */
+	/* 				'method' => 'get', */
+	/* 				'return' => 'view', */
+	/* 			) */
+	/* 		); */
+	/* 		$this->assertTextEquals('view', $this->controller->view); */
+	/* 	} */
 
-/**
- * Expect visitor can access view action by json
- *
- * @return void
- */
-	public function testViewJson() {
-		$ret = $this->testAction(
-			'/announcements/announcements/view/1.json',
-			array(
-				'method' => 'get',
-				'type' => 'json',
-				'return' => 'contents',
-			)
-		);
-		$result = json_decode($ret, true);
+	/* /\** */
+	/*  * Expect visitor can access view action by json */
+	/*  * */
+	/*  * @return void */
+	/*  *\/ */
+	/* 	public function testViewJson() { */
+	/* 		$ret = $this->testAction( */
+	/* 			'/announcements/announcements/view/1.json', */
+	/* 			array( */
+	/* 				'method' => 'get', */
+	/* 				'type' => 'json', */
+	/* 				'return' => 'contents', */
+	/* 			) */
+	/* 		); */
+	/* 		$result = json_decode($ret, true); */
 
-		$this->assertTextEquals('view', $this->controller->view);
-		$this->assertArrayHasKey('code', $result, print_r($result, true));
-		$this->assertEquals(200, $result['code'], print_r($result, true));
-	}
+	/* 		$this->assertTextEquals('view', $this->controller->view); */
+	/* 		$this->assertArrayHasKey('code', $result, print_r($result, true)); */
+	/* 		$this->assertEquals(200, $result['code'], print_r($result, true)); */
+	/* 	} */
 
-/**
- * Expect admin user can access view action
- *
- * @return void
- */
-	public function testViewByAdmin() {
-		$this->_generateController('Announcements.Announcements');
-		$this->_loginAdmin();
+	/* /\** */
+	/*  * Expect admin user can access view action */
+	/*  * */
+	/*  * @return void */
+	/*  *\/ */
+	/* 	public function testViewByAdmin() { */
+	/* 		$this->_generateController('Announcements.Announcements'); */
+	/* 		$this->_loginAdmin(); */
 
-		$view = $this->testAction(
-			'/announcements/announcements/view/1',
-			array(
-				'method' => 'get',
-				'return' => 'view',
-			)
-		);
+	/* 		$view = $this->testAction( */
+	/* 			'/announcements/announcements/view/1', */
+	/* 			array( */
+	/* 				'method' => 'get', */
+	/* 				'return' => 'view', */
+	/* 			) */
+	/* 		); */
 
-		$this->assertTextContains('nc-announcements-1', $view, print_r($view, true));
-		$this->assertTextContains('ng-controller="Announcements"', $view, print_r($view, true));
+	/* 		$this->assertTextContains('nc-announcements-1', $view, print_r($view, true)); */
+	/* 		$this->assertTextContains('ng-controller="Announcements"', $view, print_r($view, true)); */
 
-		$this->_logout();
-	}
+	/* 		$this->_logout(); */
+	/* 	} */
 
-/**
- * Expect user cannot access view action with unknown frame id
- *
- * @return void
- */
-	public function testViewByUnkownFrameId() {
-		$this->setExpectedException('InternalErrorException');
-		$this->testAction(
-			'/announcements/announcements/view/999',
-			array(
-				'method' => 'get',
-				'return' => 'view',
-			)
-		);
-	}
+	/* /\** */
+	/*  * Expect user cannot access view action with unknown frame id */
+	/*  * */
+	/*  * @return void */
+	/*  *\/ */
+	/* 	public function testViewByUnkownFrameId() { */
+	/* 		$this->setExpectedException('InternalErrorException'); */
+	/* 		$this->testAction( */
+	/* 			'/announcements/announcements/view/999', */
+	/* 			array( */
+	/* 				'method' => 'get', */
+	/* 				'return' => 'view', */
+	/* 			) */
+	/* 		); */
+	/* 	} */
 
-/**
- * Expect admin user can access edit action
- *
- * @return void
- */
-	public function testEditGet() {
-		$this->_generateController('Announcements.Announcements');
-		$this->_loginAdmin();
+	/* /\** */
+	/*  * Expect admin user can access edit action */
+	/*  * */
+	/*  * @return void */
+	/*  *\/ */
+	/* 	public function testEditGet() { */
+	/* 		$this->_generateController('Announcements.Announcements'); */
+	/* 		$this->_loginAdmin(); */
 
-		$this->testAction(
-			'/announcements/announcements/edit/1',
-			array(
-				'method' => 'get',
-				'return' => 'contents'
-			)
-		);
+	/* 		$this->testAction( */
+	/* 			'/announcements/announcements/edit/1', */
+	/* 			array( */
+	/* 				'method' => 'get', */
+	/* 				'return' => 'contents' */
+	/* 			) */
+	/* 		); */
 
-		$this->assertTextEquals('edit', $this->controller->view);
+	/* 		$this->assertTextEquals('edit', $this->controller->view); */
 
-		$this->_logout();
-	}
+	/* 		$this->_logout(); */
+	/* 	} */
 
-/**
- * Expect view action to be successfully handled w/ null frame.block_id
- * This situation typically occur after placing new plugin into page
- *
- * @return void
- */
-	public function testAddFrameWithoutBlock() {
-		$this->testAction(
-			'/announcements/announcements/view/3',
-			array(
-				'method' => 'get',
-				'return' => 'contents'
-			)
-		);
-		$this->assertTextEquals('view', $this->controller->view);
-	}
+	/* /\** */
+	/*  * Expect view action to be successfully handled w/ null frame.block_id */
+	/*  * This situation typically occur after placing new plugin into page */
+	/*  * */
+	/*  * @return void */
+	/*  *\/ */
+	/* 	public function testAddFrameWithoutBlock() { */
+	/* 		$this->testAction( */
+	/* 			'/announcements/announcements/view/3', */
+	/* 			array( */
+	/* 				'method' => 'get', */
+	/* 				'return' => 'contents' */
+	/* 			) */
+	/* 		); */
+	/* 		$this->assertTextEquals('view', $this->controller->view); */
+	/* 	} */
 
 /**
  * Expect admin user can publish announcements
