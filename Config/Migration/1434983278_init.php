82c5df25048c971e430b1912eaa5cc0c536cb6d7
s-nakajima <nakajimashouhei@gmail.com>
2015-08-05 11:55:21 +0900

    Fixed initial data generation of Migration

--- Config/Migration/1434983278_init.php
+++ Config/Migration/1434983278_init.php
@@ -8,13 +8,15 @@
  * @copyright Copyright 2014, NetCommons Project
  */
 
+App::uses('NetCommonsMigration', 'NetCommons.Config/Migration');
+
 /**
  * Init migration
  *
  * @author Shohei Nakajima <nakajimashouhei@gmail.com>
  * @package NetCommons\Announcements\Config\Migration
  */
-class Init extends CakeMigration {
+class Init extends NetCommonsMigration {
 
 /**
  * Migration description
@@ -85,29 +87,6 @@ class Init extends CakeMigration {
 			),
 		),
 
-		'Plugin' => array(
-			array(
-				'language_id' => 2,
-				'key' => 'announcements',
-				'namespace' => 'netcommons/announcements',
-				'name' => 'お知らせ',
-				'type' => 1,
-				'default_action' => 'announcements/view',
-				'default_setting_action' => 'announcement_blocks/index',
-			),
-		),
-		'PluginsRole' => array(
-			array(
-				'role_key' => 'room_administrator',
-				'plugin_key' => 'announcements'
-			),
-		),
-		'PluginsRoom' => array(
-			array(
-				'room_id' => '1',
-				'plugin_key' => 'announcements'
-			),
-		),
 	);
 
 /**
@@ -133,26 +112,6 @@ class Init extends CakeMigration {
 
 		foreach ($this->records as $model => $records) {
 			if (!$this->updateRecords($model, $records)) {
-				return false;
-			}
-		}
-
-		return true;
-	}
-
-/**
- * Update model records
- *
- * @param string $model model name to update
- * @param string $records records to be stored
- * @param string $scope ?
- * @return bool Should process continue
- */
-	public function updateRecords($model, $records, $scope = null) {
-		$Model = $this->generateModel($model);
-		foreach ($records as $record) {
-			$Model->create();
-			if (!$Model->save($record, false)) {
 				return false;
 			}
 		}
