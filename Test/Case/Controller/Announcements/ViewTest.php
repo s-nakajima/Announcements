f15f574e7b737d83464b24673cac369c70eb2174
s-nakajima <nakajimashouhei@gmail.com>
2015-09-24 14:34:56 +0900

    テスト見直しに関する修正

---
+++ Test/Case/Controller/Announcements/ViewTest.php
@@ -0,0 +1,152 @@
+<?php
+/**
+ * AnnouncementsController Test Case
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('AnnouncementsController', 'Announcements.Controller');
+App::uses('AnnouncementsControllerTestBase', 'Announcements.Test/Case/Controller');
+App::uses('WorkflowContentViewTest', 'Workflow.TestSuite');
+
+/**
+ * AnnouncementsController Test Case
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Announcements\Test\Case\Controller
+ */
+class ViewTest extends AnnouncementsControllerTestBase implements WorkflowContentViewTest {
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+
+		$this->generate('Announcements.Announcements', array(
+			'components' => array(
+				'Auth' => array('user'),
+				'Session',
+				'Security',
+			)
+		));
+	}
+
+/**
+ * view()のテスト(ログインなし)
+ *
+ * @return void
+ */
+	public function testView() {
+		//アクション実行
+		$frameId = '6';
+		$result = $this->testAction('/announcements/announcements/view/' . $frameId, array(
+			'method' => 'get',
+			'return' => 'view',
+		));
+
+		//評価
+		$this->assertNotEmpty($result);
+		$this->assertTextNotContains('/announcements/announcements/edit/' . $frameId, $result);
+	}
+
+/**
+ * view()のテスト(作成権限あり)
+ *
+ * @return void
+ */
+	public function testViewByCreatable() {
+		AuthGeneralTestSuite::login($this, Role::ROOM_ROLE_KEY_GENERAL_USER);
+
+		$this->testView();
+
+		AuthGeneralTestSuite::logout($this);
+	}
+
+/**
+ * view()のテスト(編集権限あり)
+ *
+ * @return void
+ */
+	public function testViewByEditable() {
+		AuthGeneralTestSuite::login($this);
+
+		//アクション実行
+		$frameId = '6';
+		$result = $this->testAction('/announcements/announcements/view/' . $frameId, array(
+			'method' => 'get',
+			'return' => 'view',
+		));
+
+		//評価
+		$this->assertNotEmpty($result);
+		$this->assertTextContains('/announcements/announcements/edit/' . $frameId, $result);
+
+		AuthGeneralTestSuite::logout($this);
+	}
+
+/**
+ * view()のコンテンツなしテスト
+ *
+ * @return void
+ */
+	public function testViewNoContent() {
+		//アクション実行
+		$frameId = '14';
+		$result = $this->testAction('/announcements/announcements/view/' . $frameId, array(
+			'method' => 'get',
+			'return' => 'view',
+		));
+
+		//評価
+		$this->assertEmpty($result);
+	}
+
+/**
+ * view()のコンテンツなしテスト(編集権限あり)
+ *
+ * @return void
+ */
+	public function testViewNoContentByEditable() {
+		AuthGeneralTestSuite::login($this);
+
+		//アクション実行
+		$frameId = '14';
+		$result = $this->testAction('/announcements/announcements/view/' . $frameId, array(
+			'method' => 'get',
+			'return' => 'view',
+		));
+
+		//評価
+		$this->assertNotEmpty($result);
+		$this->assertTextContains('/announcements/announcements/edit/' . $frameId, $result);
+
+		AuthGeneralTestSuite::logout($this);
+	}
+
+/**
+ * view()のフレーム削除テスト
+ *
+ * @return void
+ */
+	public function testViewOnDeleteFrame() {
+		//アクション実行
+		$frameId = '12';
+		$result = $this->testAction('/announcements/announcements/view/' . $frameId, array(
+			'method' => 'get',
+			'return' => 'view',
+		));
+
+		//評価
+		$this->assertNotEmpty($result);
+		$this->assertTextNotContains('/announcements/announcements/edit/' . $frameId, $result);
+	}
+
+
+}
