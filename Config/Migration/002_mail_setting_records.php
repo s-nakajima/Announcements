032c14132538d03fd605bdec10f5a09faa1cf14b
s-nakajima <nakajimashouhei@gmail.com>
2016-05-23 14:28:44 +0900

    デフォルト、メール送信とする

--- Config/Migration/002_mail_setting_records.php
+++ Config/Migration/002_mail_setting_records.php
@@ -52,7 +52,7 @@ class BbsMailSettingRecords extends MailsMigration {
 			array(
 				'plugin_key' => self::PLUGIN_KEY,
 				'block_key' => null,
-				'is_mail_send' => false,
+				'is_mail_send' => true,
 			),
 		),
 		'MailSettingFixedPhrase' => array(
